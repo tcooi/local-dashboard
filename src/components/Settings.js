46fe74487094c5752fcf6e25df67bdae0e2de582
tcooi <tjanchao.ooi@gmail.com>
2020-08-02 14:46:04 +0200

    added timezone wiki link

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -7,21 +7,25 @@ const Settings = ({ data, handleSubmit, handleInputChange }) => {
             <div>
                 Time
                 <form onSubmit={handleSubmit}>
-
-                    <input type='text' name='timezone' value={data.timezone} onChange={handleInputChange}/> <button>save</button>
+                    <input 
+                        type='text' 
+                        name='timezone' 
+                        value={data.timezone} 
+                        onChange={handleInputChange}
+                    /> 
+                    <button>save</button> <br/>
+                    https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
                 </form>
             </div>
             <div>
                 Weather
                 <form onSubmit={handleSubmit}>
-
                     <input type='text' name='city' value={data.city} onChange={handleInputChange}/> <button>save</button>
                 </form>
             </div>
             <div>
                 Public transport
                 <form onSubmit={handleSubmit}>
-
                     <input type='text'/> <button>save</button>
                 </form>
             </div>
